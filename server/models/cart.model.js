70013cdcbf5d31ce22bfde4e7d71d0e47084b9eb
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-29 13:46:07 +0530

    add cartSchema

--- server/models/cart.model.js
+++ server/models/cart.model.js
@@ -1,9 +1,32 @@
-import {Schema, model} from 'mongoose'
+import { Schema, model } from "mongoose";
 
-const cartSchema = new Schema({
-
+const cartSchema = new Schema(
+  {
     userId: {
-        
-    }
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    courses: [
+      {
+        courseId: {
+          type: Schema.Types.ObjectId,
+          ref: "Course",
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
 
-})
+export const Cart = model("Cart", cartSchema);
