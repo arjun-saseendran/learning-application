84359b1fd1e3b1ae8ccdf5ecba173d2ee17173bc
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-29 14:41:44 +0530

    finish index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
-import express from 'express'
+import express from "express";
+import { connectDB } from "../server/config/db.js";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  connectDB();
+
+  console.log(`Server running on ${port}`);
+});
