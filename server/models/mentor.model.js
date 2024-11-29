6484514698c41db35b89dd5fa534cb5a89b4c9ee
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-29 14:14:09 +0530

    add mentorSchema

---
+++ server/models/mentor.model.js
@@ -0,0 +1,42 @@
+import { Schema, model } from "mongoose";
+
+const mentorSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      enum: ["mentor", "admin"],
+      default: "mentor",
+    },
+    password: {
+      type: String,
+      required: true,
+      miniLength: 6,
+    },
+    profilePic: {
+      type: String,
+      default:
+        "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg",
+    },
+    qualification: {
+      type: String,
+    },
+    courses: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Mentor = model('Mentor', mentorSchema)
