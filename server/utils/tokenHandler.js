38fe5ce03a83e019f2829099be9d80d05327575d
arjun-saseendran <arjunsaseendran@icloud.com>
2024-12-08 08:30:46 +0530

    add generateToken

---
+++ server/utils/tokenHandler.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+
+// Generate token
+export const generateToken = (user, role) => {
+  try {
+    const token = jwt.sign({ id: user._id, role }, process.env.JWT_SECRET);
+    return token;
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .json({ error: error.message || "Internal server error" });
+  }
+};
