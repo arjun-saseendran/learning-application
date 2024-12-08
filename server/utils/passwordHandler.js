558349432a868a79a571d02a765a69c00aa1eec8
arjun-saseendran <arjunsaseendran@icloud.com>
2024-12-08 07:57:27 +0530

    add passwordHandler

---
+++ server/utils/passwordHandler.js
@@ -0,0 +1,16 @@
+import bcrypt from "bcrypt";
+
+// Hansh or Compare password
+export const passwordHandler = async (password, hashPassword = undefined) => {
+  try {
+    if (hashPassword === undefined) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      return hashPassword;
+    } else {
+      const matchedPassword = await bcrypt.compare(password, hashPassword);
+      return matchedPassword;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
