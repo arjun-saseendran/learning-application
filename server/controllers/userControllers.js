653d8e327ef3140d2c9c63cc06274204370a78c1
arjun-saseendran <arjunsaseendran@icloud.com>
2024-12-08 08:34:27 +0530

    add userSignup

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -2,36 +2,43 @@ import { User } from "../models/userModel.js";
 import { passwordHandler } from "../utils/passwordHandler.js";
 
 // User signup
-const userSignup = async(req, res) => {
+export const userSignup = async (req, res) => {
   try {
     // Destructing data from request.body
     const { name, email, mobile, password } = req.body;
 
     // Check each field not empty
-    if (!name || !email || !mobile || !password){
-      return res.status(400).json({ error: "All fields required" })};
-    
-      // Check user already exists
-      const existUser = await User.fineOne({email})
-      if(existUser){
-        return res.status(400).json({error: 'User already exists'})
-      }
+    if (!name || !email || !mobile || !password) {
+      return res.status(400).json({ error: "All fields required" });
+    }
 
-      // Hash password
-      const hashedPassword = await passwordHandler(password)
+    // Check user already exists
+    const existUser = await User.fineOne({ email });
+    if (existUser) {
+      return res.status(400).json({ error: "User already exists" });
+    }
 
-      // Create new user object
-      const newUser = new User({name, email, mobile, password: hashedPassword})
+    // Hash password
+    const hashedPassword = await passwordHandler(password);
 
-      // Save data to database
-      newUser.save()
+    // Create new user object
+    const newUser = new User({ name, email, mobile, password: hashedPassword });
 
-      // Create token
-      
+    // Save data to database
+    newUser.save();
 
-      res.status(200).json({message: 'User created successfully'})
+    // Exclude password
+    const { password: _, ...userWithoutPassword } = newUser.toObject();
+
+    res
+      .status(200)
+      .json({
+        message: "User created successfully",
+        data: userWithoutPassword,
+      });
   } catch (error) {
-    res.status(error.status || 500).json({error: error.message || 'Internal server error'})
-
+    res
+      .status(error.status || 500)
+      .json({ error: error.message || "Internal server error" });
   }
 };
